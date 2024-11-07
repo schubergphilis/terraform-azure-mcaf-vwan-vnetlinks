7c287aeda54dc61038d35e070619b1bdc24962e4
Gillian Stravers <gillianstravers@icloud.com>
2024-11-07 10:07:12 +0100

    working version

--- main.tf
+++ main.tf
@@ -3,4 +3,5 @@ resource "azurerm_virtual_hub_connection" "this" {
   name                      = var.virtual_hub_connection[each.key].vnetlink_name
   virtual_hub_id            = var.virtual_hub_connection[each.key].virtual_hub_id
   remote_virtual_network_id = var.virtual_hub_connection[each.key].remote_virtual_network_id
+  internet_security_enabled = true
 }
